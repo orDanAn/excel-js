56f7bda1fe52827252f9adb2ed795d3c71da074a
Danil <ordanan38@gmail.com>
2021-07-28 12:51:31 +0800

    finish base logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,17 +4,37 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name;
+    this.emitter = options.emitter;
+    this.unsubScribers = [];
+
+    this.prepare();
+  }
+  // создали хук перед событием init
+  prepare() {
+
   }
   // Возвращаем шаблон компоненту
   toHTML() {
     return '';
   }
+  // уведомляем слушателей про event
+  // подписываемся на события
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+  // устанавливаем слушателей
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubScribers.push(unsub);
+  }
 
+  // инициализация компнента
   init() {
     this.initDomListeners();
   }
-
+  // удаляем компонент
   destroy() {
     this.removeDomListeners();
+    this.unsubScribers.forEach(unsub => unsub());
   }
 }
